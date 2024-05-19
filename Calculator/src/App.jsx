4f4174a9de4e4985fffe8ab52801cbe789aa0f06
Ritikaa-Rani <ritikaranihbz@gmail.com>
2024-05-19 12:35:04 +0530

    Design calculator in app.jsx

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -8,7 +8,29 @@ function App() {
 
   return (
     <>
-      
+      <div className='flex justify-center items-center h-screen'>
+        <div className=' w-[325px] h-[400px]'>
+          <input type="text" className='border-2 w-[320px] h-[80px]'/>
+          <div className=''>
+            <button className='border-2 w-20 h-20'>7</button>
+            <button className='border-2 w-20 h-20'>8</button>
+            <button className='border-2 w-20 h-20'>9</button>
+            <button className='border-2 w-20 h-20'>/</button>
+            <button className='border-2 w-20 h-20'>4</button>
+            <button className='border-2 w-20 h-20'>5</button>
+            <button className='border-2 w-20 h-20'>6</button>
+            <button className='border-2 w-20 h-20'>*</button>
+            <button className='border-2 w-20 h-20'>1</button>
+            <button className='border-2 w-20 h-20'>2</button>
+            <button className='border-2 w-20 h-20'>3</button>
+            <button className='border-2 w-20 h-20'>-</button>
+            <button className='border-2 w-20 h-20'>AC</button>
+            <button className='border-2 w-20 h-20'>0</button>
+            <button className='border-2 w-20 h-20'>=</button>
+            <button className='border-2 w-20 h-20'>+</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
